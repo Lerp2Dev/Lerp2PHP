7a261353556161cca12655df4cc8f60f6818e7e6
Ikillnukes <ikillnukes1@gmail.com>
2017-11-28 19:26:59 +0100

    Added pcstatsphp

---
+++ pcstatsweb/Core.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Álvaro
+ * Date: 28/11/2017
+ * Time: 19:22
+ */
+
+require_once('../Settings.php');
+require_once('../DevProfiles.php');
+require_once('../Classes/Debug.php');
+require_once('../Classes/Database.php');
+require_once('../Classes/AppLogger.php');
+
+class PCCore extends Core
+{
+
+}
